049dc66ed011d3b7463e0d1f8747f6f2fcfdc51e
Marius Skaar Ludvigsen <marius.ludvigsen@gmail.com>
2020-10-23 11:08:26 +0200

    make demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { HeartRate, PowerMeter, SpeedAndCadence } from 'web-bluetooth-training-sensors';
 import logo from './logo.svg';
 import './App.css';
 
+const setHR = async (hr, setState) => {
+  setState({
+    hr
+  })
+}
 function App() {
+  const [state, setState] = useState({hr: 0, power: 0, cadence: 0});
+  const hrSensor = new HeartRate();
+  const powerMeter = new PowerMeter();
+  const cadenceSensor = new SpeedAndCadence();
+
+  const setCadence = (cadence) => setState({cadence});
+  const setHr = (hr) => setState({hr});
+  const setPower = (power) => setState({power});
+  
+  const handleClick = (sensor, setState) => async () => {
+    // NOTE: Connect MUST be called on user event e.g. onClick
+    await sensor.connect();
+    sensor.addListener((value) => {
+      setState(value);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{fontSize: '46px'}} className="App">
+      <button onClick={handleClick(powerMeter, setPower)}>Connect Trainer</button>
+      <button onClick={handleClick(hrSensor, setHr)}>Connect Heart Rate Sensor</button>
+      <button onClick={handleClick(cadenceSensor, setCadence)}>Connect Cadence Sensor</button>
+      <h1>HR:      {state.hr}</h1>
+      <h1>POWER:   {state.power}</h1>
+      <h1>CADENCE: {state.cadence}</h1>
     </div>
   );
 }
